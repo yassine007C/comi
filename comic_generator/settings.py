ff80f856d7e10b966a38ddcbde8012c8bd092239
yassine007C <104086968+yassine007C@users.noreply.github.com>
2025-11-03 00:31:37 +0100

    Update settings.py

--- comic_generator/settings.py
+++ comic_generator/settings.py
@@ -106,11 +106,15 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 '''
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 '''
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
+
+
 
 STORAGES = {
     
