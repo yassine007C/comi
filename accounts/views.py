8baaf4133fdd7cf4bbc813eef9d40d0654d5c862
yassine007C <104086968+yassine007C@users.noreply.github.com>
2025-11-03 19:17:43 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import CustomUserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
