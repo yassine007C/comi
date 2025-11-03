97e074252dc7d61089a433306c802a421b5445b3
yassine007C <104086968+yassine007C@users.noreply.github.com>
2025-11-03 19:20:02 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ def register_view(request):
         return redirect('generator:dashboard')
     
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
@@ -19,7 +19,7 @@ def register_view(request):
         else:
             messages.error(request, 'Please correct the errors below.')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     
     return render(request, 'accounts/register.html', {'form': form})
 
