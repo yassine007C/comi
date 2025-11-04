e50b4edf36610213f3a4185542a0c4bb8a7b5b55
yassine007C <104086968+yassine007C@users.noreply.github.com>
2025-11-05 00:21:23 +0100

    Update sitemap.py

--- sitemap.py
+++ sitemap.py
@@ -1,5 +1,5 @@
 from django.contrib.sitemaps import Sitemap
-from generator.models import GeneratedImage
+
 from accounts.models import UserProfile
 
 
@@ -20,17 +20,4 @@ class AccountsSitemap(Sitemap):
         return f"/accounts/profile/{obj.user.username}/"  # Adjust as per your URL pattern
 
 
-class GeneratedImageSitemap(Sitemap):
-    changefreq = "monthly"
-    priority = 0.5
 
-    def items(self):
-        return GeneratedImage.objects.all()
-
-    def lastmod(self, obj):
-        return obj.created_at
-
-    def location(self, obj):
-        # If you have a URL pattern for each generated image, use it here.
-        # If you don't have get_absolute_url on GeneratedImage, define your URL manually. Example:
-        return f"/generated_images/{obj.pk}/"
