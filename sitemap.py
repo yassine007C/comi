4c24bd36ed7e3574f25bd319d45d2037b37d6894
yassine007C <104086968+yassine007C@users.noreply.github.com>
2025-11-04 20:54:15 +0100

    Update sitemap.py

--- sitemap.py
+++ sitemap.py
@@ -1,23 +1,23 @@
 from django.contrib.sitemaps import Sitemap
-from .models import Rest_framework, Accounts, Generator
+from .models import rest_framework, accounts, generator
 
 class Rest_frameworkSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
 
     def items(self):
-        return Rest_framework.objects.all()
+        return rest_framework.objects.all()
 
     def lastmod(self, obj):
         return obj.updated_at  # Or the relevant datetime field
 
 
-class Accounts(Sitemap):
+class AccountsSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.5
 
     def items(self):
-        return UserProfile.objects.all()
+        return accounts.objects.all()
 
     def lastmod(self, obj):
         return obj.updated_at  # Adjust if you use a different timestamp field
@@ -25,12 +25,12 @@ class Accounts(Sitemap):
     def location(self, obj):
         return obj.get_absolute_url()  # You need get_absolute_url() on this model too
 
-class Generator(Sitemap):
+class GeneratorSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.5
 
     def items(self):
-        return Generator.objects.all()
+        return generator.objects.all()
 
     def lastmod(self, obj):
         return obj.updated_at  # Adjust if you use a different timestamp field
