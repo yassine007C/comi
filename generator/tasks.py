7d56170a4da8b1f359eaf5e5de993d869015c54b
yassine007C <104086968+yassine007C@users.noreply.github.com>
2025-11-06 00:14:43 +0100

    Update tasks.py

--- generator/tasks.py
+++ generator/tasks.py
@@ -3,7 +3,8 @@ from celery import shared_task
 from django.contrib.auth import get_user_model
 from .models import GeneratedImage
 import os
-from .views import create_image_description_from_dialogue, MultiModalConversation
+from .views import MultiModalConversation
+from .image_utils import create_image_description_from_dialogue
 from django.conf import settings
 
 @shared_task
