ce9abc1f34945b5d14b3796f3beebe95e303374f
yassine007C <104086968+yassine007C@users.noreply.github.com>
2025-11-06 00:10:43 +0100

    Create image_utils.py

---
+++ generator/image_utils.py
@@ -0,0 +1,8 @@
+import dashscope
+from dashscope import MultiModalConversation
+from google import genai
+from google.genai import types
+
+def create_image_description_from_dialogue(context, dialogue, target_line, model_name='gemini-2.5-flash'):
+    # Your function body here...
+    pass
